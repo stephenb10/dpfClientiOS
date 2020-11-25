178df00d73fd1d0d39ea1dc44cea4e733d542de5
Stephen Byatt <stephen.byatt10@gmail.com>
2020-11-26 10:34:52 +1100

    Initial Commit

---
+++ Shared/DigiFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiFrameApp.swift
+//  Shared
+//
+//  Created by Stephen Byatt on 26/11/20.
+//
+
+import SwiftUI
+
+@main
+struct DigiFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
